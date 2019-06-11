f5b9d3d3c15ca605ddb63cc42e39e6cf6457f1ae
Truegolliath <truegolliath@sd-56532.dedibox.fr>
2019-06-11 12:17:44 +0200

    Initialisation

---
+++ io_shared.py
@@ -0,0 +1,22 @@
+import os, json
+
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def ensure_file(filename):
+    ensure_dir(filename)
+    # create file is not exist
+    with open(filename,"w") as f:
+        f.write("")
+
+def save_as_json(filename,data,verbose): 
+    ensure_file(filename)
+    if verbose:
+        print('Write json file to %s'%filename)
+
+    # Writing JSON data
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
